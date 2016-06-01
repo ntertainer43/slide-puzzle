6656c575aa495b09a33a2e0238e847e73a5f4643
ntertainer43 <nimeshism@gmail.com>
2016-06-01 15:16:41 +0545

    Update slide.py

--- slide.py
+++ slide.py
@@ -15,7 +15,7 @@ class Puzzle_box:
         textRect = textSurf.get_rect()
         textRect.center = self.position[0]*100 +50, self.position[1]*100 +50
         SURFACE.blit(textSurf, textRect)
-        print( self.num , textRect.center)
+
 
     def pos_update(self, last):
         if self.position == (blankx, blanky):
@@ -25,7 +25,7 @@ class Puzzle_box:
 
 
 def main():
-    global SURFACE, FONT, TILECOLOR, blankx, blanky, last
+    global SURFACE, FONT, TILECOLOR, BGCOLOR, blankx, blanky, last
 
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
@@ -49,7 +49,7 @@ def main():
     blankx, blanky = list_of_position[0]
     last = blankx, blanky
 
-    print(list_of_position)
+
     for box in list_of_boxes:
         box.position = list_of_position[num-1]
         num -=1
@@ -81,8 +81,11 @@ def main():
         for new_Box in list_of_boxes:
             new_Box.pos_update(last)
             new_Box.display()
+        win = gamewin(list_of_boxes)
+        gamewinAnimation(win)
+        print(win)
         pygame.display.update()
-        print(blankx, blanky)
+
         last = (blankx, blanky)
         FPSCLOCK.tick(15)
 
@@ -98,6 +101,30 @@ def isValidMove(blankx, blanky, dire):
         return  False
     else:
         return True
+
+def gamewin(list_of_boxes):
+    game_state= [ boxes.position for boxes in list_of_boxes]
+    game_state.append((4,4))
+    wingame = []
+    for i in range(1,5):
+        for j in range(1,5):
+
+            wingame.append((j,i))
+    if game_state == wingame:
+        return True
+
+    return False
+
+
+def gamewinAnimation(win):
+    if win == True:
+
+        FONT2 = pygame.font.Font('freesansbold.ttf', 40)
+        textwin = FONT2.render(str('You have won the game'), True, (255, 200, 100))
+        textwinRect = textwin.get_rect()
+        textwinRect.center = 300, 300
+        SURFACE.blit(textwin, textwinRect)
+
 
 
 if __name__ == '__main__':
